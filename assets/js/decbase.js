829da828020e5796d6e28e3dd4be0cd6954a5c6c
rashadulalam <rashadul91@gmail.com>
2022-12-10 10:54:26 +0600

    Service area

--- assets/js/decbase.js
+++ assets/js/decbase.js
@@ -1,16 +1,38 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// });
+
+$(document).ready(function() {
+    $('.service-list').owlCarousel({
+        loop: true,
+        margin: 1,
+        autoplay: true,
+        nav: true,
+        dots: false,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
         }
-    }
-});
+    })
+})
